a49941e6723de32275385d381e59e9da73c2bc04
pong34811 <pong34811@gmail.com>
2024-07-27 11:08:56 +0700

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,76 @@
+
+
+/// class ตอนเช้า
+
+// console.log("hello world");
+
+// cd "ชื่อ path ปลายทาง"
+
+// cd ..
+
+// dir
+
+/*
+วิธี commit message 2 รูป
+1. ใส่ //
+2. /*
+    ฟหกฟหก    
+*/
+
+/*
+let a = 10;
+a = 20;
+console.log(a);
+*/
+
+
+/*
+const b = 10;
+b = 20;
+console.log(b);
+*/
+
+/* ตัวแปร const ห้ามใช้ในการแก้ไขค่า
+TypeError: Assignment to constant variable.
+    at Object.<anonymous> (C:\Wed\class1-nodejs\hello.js:26:3)
+    at Module._compile (node:internal/modules/cjs/loader:1504:14)
+    at Module._extensions..js (node:internal/modules/cjs/loader:1588:10)
+    at Module.load (node:internal/modules/cjs/loader:1282:32)
+    at Module._load (node:internal/modules/cjs/loader:1098:12)
+    at TracingChannel.traceSync (node:diagnostics_channel:315:14)
+    at wrapModuleLoad (node:internal/modules/cjs/loader:215:24)
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:158:5)
+    at node:internal/main/run_main_module:30:49
+*/
+
+/*
+let x = 10;
+x = 1.5;
+let y = 20;
+let z = x+y ;
+
+console.log(z); // 21.5
+*/
+/*
+let x1 = "Hello";
+let y1 = "WORLD";
+let z1 = `say, ${x} ${y};
+
+console.log(z1); // say, Hello WORLD;
+*/
+ 
+/*
+const a = 10
+const b ="20"
+const c = a+ b
+console.log(c)
+*/
+
+let nameCheacked = true;
+let phoneCheaked = false;
+console.log(`Name Checked : ${nameCheacked} Phone Checked : ${phoneCheaked}`);
+console.log(5 > 10);
+
+
+
+
